01d69a2709e561f28451ad3c727b06c7ea1bf000
abfkh <84881348+abfkh@users.noreply.github.com>
2022-12-11 15:50:39 +0530

    Updates

---
+++ precache-manifest.efe620873265cfc1be45453a8a975a41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231dd7b1747d5c6e494268e3074e7393",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4fb2a9df5b5b6bb3c3e",
+    "url": "/static/css/main.794fba2e.chunk.css"
+  },
+  {
+    "revision": "c3e8b70c6c78cf3655b0",
+    "url": "/static/js/2.42609b22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42609b22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4fb2a9df5b5b6bb3c3e",
+    "url": "/static/js/main.f0e92bd2.chunk.js"
+  },
+  {
+    "revision": "40c733a7d94127bd1e95",
+    "url": "/static/js/runtime-main.7fd0c590.js"
+  }
+]);
